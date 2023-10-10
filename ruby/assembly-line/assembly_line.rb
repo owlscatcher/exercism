b8b842f8a4b57a929265e36dc471de5a33328ac4
Alexander Veselov <collabsis.g@gmail.com>
2023-10-10 08:01:30 +0400

    Submited assembly-line exercise

--- ruby/assembly-line/assembly_line.rb
+++ ruby/assembly-line/assembly_line.rb
@@ -2,13 +2,27 @@ class AssemblyLine
   def initialize(speed)
     @speed = speed
     @prod_per_hour = 221
+    @rate_hash = {
+      1 => 1.0,
+      2 => 1.0,
+      3 => 1.0,
+      4 => 1.0,
+      5 => 0.9,
+      6 => 0.9,
+      7 => 0.9,
+      8 => 0.9,
+      9 => 0.8,
+      10 => 0.77
+    }
   end
 
   def production_rate_per_hour
-    @speed * @prod_per_hour
+    @speed * @prod_per_hour * @rate_hash[@speed]
   end
 
   def working_items_per_minute
-
+    (production_rate_per_hour / 60).floor
   end
+
+
 end
