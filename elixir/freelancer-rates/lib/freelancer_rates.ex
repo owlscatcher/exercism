d5088986871544ea7821e2b99092420593bfd276
Alexander Veselov <collabsis.g@gmail.com>
2025-02-25 15:43:31 +0400

    Success freelancer-rates

--- elixir/freelancer-rates/lib/freelancer_rates.ex
+++ elixir/freelancer-rates/lib/freelancer_rates.ex
@@ -1,17 +1,29 @@
 defmodule FreelancerRates do
-  def daily_rate(hourly_rate) do
-    # Please implement the daily_rate/1 function
-  end
+  @ballable_days_per_month 22
+  @hour_per_work_day 8.0
 
-  def apply_discount(before_discount, discount) do
-    # Please implement the apply_discount/2 function
-  end
+  def daily_rate(hourly_rate),
+    do: hourly_rate * @hour_per_work_day
+
+  def apply_discount(before_discount, discount),
+    do: before_discount * to_coeff(discount)
 
   def monthly_rate(hourly_rate, discount) do
-    # Please implement the monthly_rate/2 function
+    (daily_rate(hourly_rate) * @ballable_days_per_month)
+    |> apply_discount(discount)
+    |> ceil()
   end
 
   def days_in_budget(budget, hourly_rate, discount) do
-    # Please implement the days_in_budget/3 function
+    hourly_rate
+    |> daily_rate()
+    |> apply_discount(discount)
+    |> calculate_available_days(budget)
+    |> Float.floor(1)
   end
+
+  defp to_coeff(value), do: (100 - value) / 100.0
+
+  defp calculate_available_days(discounted_day_rate, budget),
+    do: budget / discounted_day_rate * 1.0
 end
